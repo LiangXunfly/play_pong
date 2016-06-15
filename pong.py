d6530ecf3d1ce6c435d4004246fe3081ac61f502
LiangXunfly <LiangXunfly@outlook.com>
2016-06-15 09:26:56 +0800

    add lives and display it

--- pong.py
+++ pong.py
@@ -11,9 +11,11 @@ class Ball(pygame.sprite.Sprite):
 		self.speed = speed
 
 	def move(self):
+		global points
 		if self.rect.left < 0 or self.rect.right > screen.get_width():
 			self.speed[0] = -self.speed[0]
 		if self.rect.top < 0:
+			points = points + 1
 			self.speed[1] = -self.speed[1]
 		self.rect = self.rect.move(self.speed)
 
@@ -35,10 +37,20 @@ ball = Ball(r'source\images\wackyball.bmp', [50, 50], [10, 5])
 group = pygame.sprite.Group(ball)
 paddle = Paddle([270, 400])
 
+#version2 add
+points = 0
+lives = 3
+
 if __name__ == '__main__':
 	while True:
 		clock.tick(30)
 		screen.fill([255,255, 255])
+
+		#version2 add
+		font = pygame.font.Font(None, 50)
+		score_text = font.render(str(points), 1, (0, 0, 0))
+		textpos = (10, 10)
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
@@ -49,9 +61,20 @@ if __name__ == '__main__':
 		ball.move()
 		screen.blit(ball.image, ball.rect)
 		screen.blit(paddle.image, paddle.rect)
+
+		#version2 add
+		screen.blit(score_text, textpos)
+		#for i in range(lives):
+		for i in range(lives - 1):
+			screen.blit(ball.image, (screen.get_width() - 30 * i - 20, 20))
+
 		pygame.display.flip()
 
-		if ball.rect.bottom >= screen.get_size()[1]:
-			result = easygui.msgbox('You lost!')
-			if result == 'OK':
+		if ball.rect.bottom >= screen.get_rect().bottom:
+			if lives == 1:
+				result = easygui.msgbox('Your score is %d!' % points)
 				sys.exit()
+			else:
+				lives = lives - 1
+				pygame.time.delay(2000)
+				ball.rect.topleft = [50, 50]
